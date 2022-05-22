f867733f2dfcacd383d9cc19dcaec519c1d663d8
Vali Sumurduc <vali.sumurduc@gmail.com>
2022-05-22 20:11:15 +0200

    Minor changes

--- src/services/api.js
+++ src/services/api.js
@@ -2,15 +2,21 @@ import { get } from 'axios'
 import { TOKEN } from '../constants'
 
 export const getBranch = () =>
-  get('https://api.github.com/repos/argoproj/argo-site/branches/master', {
-    headers: {
-      Authorization: `token ${TOKEN}`,
-    },
-  })
+  get(
+    'https://api.github.com/repos/argoproj/argo-site/branches/master',
+    TOKEN && {
+      headers: {
+        Authorization: `token ${TOKEN}`,
+      },
+    }
+  )
 
 export const getTrees = sha =>
-  get(`https://api.github.com/repos/argoproj/argo-site/git/trees/${sha}`, {
-    headers: {
-      Authorization: `token ${TOKEN}`,
-    },
-  })
+  get(
+    `https://api.github.com/repos/argoproj/argo-site/git/trees/${sha}`,
+    TOKEN && {
+      headers: {
+        Authorization: `token ${TOKEN}`,
+      },
+    }
+  )
